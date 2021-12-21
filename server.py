6d39182a0d4e2a0357f785a355265030f5b8e9c1
Chris Tsantiris <chris.tsantiris@gmail.com>
2021-12-21 14:30:18 -0500

    Client receives message from server

--- server.py
+++ server.py
@@ -9,12 +9,21 @@ SIZE = 1024
 FORMAT = "utf-8"
 SERVER_DATA_PATH = "server_data"
 
+def handle_client(conn, addr):
+    print(f"[New Connection] {addr} connected.")
+    conn.send("OK@Welcome to the File Server".encode(FORMAT))
+
 def main():
     print("[Starting] Server is starting.")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
     server.listen()
     print("[Listening] Server is listening")
+
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
 
 if __name__ == "__main__":
         main()
